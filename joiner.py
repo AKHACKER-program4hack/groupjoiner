07ad4e28f7394fc7643691265db9a09e08dbfe2c
AKHACKER-program4hack <akhacker.program4hack@gmail.com>
2021-09-12 14:24:31 -0700

    Updated

--- joiner.py
+++ joiner.py
@@ -16,9 +16,9 @@ with TelegramClient('anon', api_id, api_hash) as Client:
                 groupstojoin = f.read().splitlines()
                 for group in groupstojoin:
                     # group = "https://t.me/test123456gagaha"
-                    if len(joinedgroups) == 3:
+                    if len(joinedgroups) == 10:
                         break
-                    elif len(joinedgroups) != 3:
+                    elif len(joinedgroups) != 10:
                         n = await Client(JoinChannelRequest(group))
                         if n.updates:
                             print("Joined")
@@ -39,6 +39,3 @@ with TelegramClient('anon', api_id, api_hash) as Client:
             print(error)
 
     Client.run_until_disconnected()
-
-
-#Updates(updates=[], users=[], chats=[Channel(id=1567490931, title='1', photo=ChatPhotoEmpty(), date=datetime.datetime(2021, 9, 10, 13, 28, 50, tzinfo=datetime.timezone.utc), version=0, creator=False, left=False, broadcast=False, verified=False, megagroup=True, restricted=False, signatures=False, min=False, scam=False, has_link=False, has_geo=False, slowmode_enabled=True, access_hash=8539590535293599179, username='test123456gagaha', restriction_reason=[], admin_rights=None, banned_rights=None, default_banned_rights=ChatBannedRights(until_date=datetime.datetime(2038, 1, 19, 3, 14, 7, tzinfo=datetime.timezone.utc), view_messages=False, send_messages=False, send_media=False, send_stickers=False, send_gifs=False, send_games=False, send_inline=False, embed_links=False, send_polls=False, change_info=True, invite_users=False, pin_messages=True), participants_count=None)], date=datetime.datetime(2021, 9, 10, 13, 29, 42, tzinfo=datetime.timezone.utc), seq=0)
